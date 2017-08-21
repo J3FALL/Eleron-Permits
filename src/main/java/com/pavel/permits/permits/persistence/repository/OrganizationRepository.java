dd3ea76923e0bbad6141f4699c341581cb8ef154
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-08-21 20:31:38 +0300

    Change CrudRepository to JpaRepository

--- src/main/java/com/pavel/permits/permits/persistence/repository/OrganizationRepository.java
+++ src/main/java/com/pavel/permits/permits/persistence/repository/OrganizationRepository.java
@@ -2,11 +2,11 @@ package com.pavel.permits.permits.persistence.repository;
 
 import com.pavel.permits.permits.model.Organization;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Pavel on 21.08.2017.
  */
-public interface OrganizationRepository extends CrudRepository<Organization, Integer> {
+public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
 
 }
--- src/main/java/com/pavel/permits/permits/persistence/repository/PersonRepository.java
+++ src/main/java/com/pavel/permits/permits/persistence/repository/PersonRepository.java
@@ -2,11 +2,11 @@ package com.pavel.permits.permits.persistence.repository;
 
 import com.pavel.permits.permits.model.Person;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Pavel on 21.08.2017.
  */
-public interface PersonRepository extends CrudRepository<Person, Integer> {
+public interface PersonRepository extends JpaRepository<Person, Integer> {
 
 }
