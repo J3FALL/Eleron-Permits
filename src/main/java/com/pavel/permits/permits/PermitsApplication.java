57e2e7d9dea8aa7700a5398d3e441964cd4edb9a
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-08-22 05:36:58 +0300

    Add ModelMapper

--- src/main/java/com/pavel/permits/permits/PermitsApplication.java
+++ src/main/java/com/pavel/permits/permits/PermitsApplication.java
@@ -1,13 +1,20 @@
 package com.pavel.permits.permits;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class PermitsApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PermitsApplication.class, args);
