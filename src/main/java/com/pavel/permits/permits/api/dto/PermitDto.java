877bc10f8ce23baa864453f983b8070991ffd529
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-08-28 11:57:04 +0300

    Extend PermitDto

--- src/main/java/com/pavel/permits/permits/api/dto/PermitDto.java
+++ src/main/java/com/pavel/permits/permits/api/dto/PermitDto.java
@@ -10,7 +10,7 @@ import lombok.Data;
 /**
  * Created by Pavel on 24.08.2017.
  */
-
+//TODO: personName = FIO
 @Data
 public class PermitDto {
 
@@ -19,11 +19,29 @@ public class PermitDto {
     @JsonProperty("person_id")
     private Integer personId;
 
+    @JsonProperty("person_first_name")
+    private String personFirstName;
+
+    @JsonProperty("person_middle_name")
+    private String personMiddleName;
+
+    @JsonProperty("person_last_name")
+    private String personLastName;
+
+    @JsonProperty("person_org")
+    private String personOrganizationName;
+
+    @JsonProperty("person_position")
+    private String personPositionName;
+
     @JsonProperty("code_ids")
     private List<Integer> codeIds;
 
     @JsonProperty("pass_id")
     private Integer passId;
+
+    @JsonProperty("pass_name")
+    private String passName;
 
     @JsonProperty("issue_date")
     private Date issueDate;
