3a930407efaab34973fe60162a07e4f0718a3e86
Pavel Vychuzhanin <crisp.coad@gmail.com>
2017-08-28 11:56:45 +0300

    Update WebAppConfig

--- src/main/java/com/pavel/permits/permits/config/WebAppConfig.java
+++ src/main/java/com/pavel/permits/permits/config/WebAppConfig.java
@@ -3,7 +3,6 @@ package com.pavel.permits.permits.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,12 +11,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * Created by Pavel on 28.08.2017.
  */
 @Configuration
-@EnableWebMvc
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/templates/**").addResourceLocations("/templates/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
     }
 
 
